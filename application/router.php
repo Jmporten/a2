f9731a3649f14017f628872828fbd3e7c097920c
jmporten <jmporten@moonami.com>
2018-04-12 13:06:10 -0400

    init

--- application/router.php
+++ application/router.php
@@ -8,14 +8,11 @@
 
 function classLoader($class) {
     $pathControl = 'application/controllers/' . strtolower($class) . '.class.php';
-    $pathLibs = 'application/libs/' . strtolower($class) . '.class.php';
     $pathModels = 'application/models/' . strtolower($class) . '.class.php';
     $path = 'application/'. strtolower($class) . '.class.php';
+
     if (file_exists($pathControl)) {
         require_once $pathControl;
-    }
-    elseif (file_exists($pathLibs)) {
-        require_once $pathLibs;
     }
     elseif (file_exists($pathModels )) {
         require_once $pathModels ;
